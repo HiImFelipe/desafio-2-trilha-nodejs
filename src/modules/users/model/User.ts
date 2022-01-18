562e12c499513bfdac8030aa96eb04996bf83218
FelipeSSDev <felipedevbrasil@gmail.com>
2022-01-18 12:36:31 -0300

    feat: Create User class implementation

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,22 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  public id: string;
+  public created_at: Date;
+  public updated_at: Date;
+  public name: string;
+  public email: string;
+  public admin = false;
+
+  constructor() {
+    this.id = uuidV4();
+    this.created_at = new Date();
+    this.updated_at = new Date();
+  }
+
+  turnIntoAdmin(): void {
+    this.admin = true;
+  }
 }
 
 export { User };
